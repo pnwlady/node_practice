21dfce278a536f73816e87c129f7c9a04d1e1a6a
heidilaursen <heidimlaursen@gmail.com>
2016-03-16 20:59:50 -0700

    Express 4.1 app

--- express_app.js
+++ express_app.js
@@ -1,6 +1,21 @@
 // <%= prints to page
+var request = require('request');
+var url = require('url');
+
 app.get(‘/tweets/:username’, function(req, response) {
-…
+
+    var username = req.param.username;
+
+    options = {
+        protocol: "http:",
+        host: 'api.twitter.com', //wont work
+        pathname: '/1statuses/user_timeline.json',
+        query: { screen_name: username, county: 10 }
+    }
+
+    var twitterUrl = url.format(options);
+    request(twitterUrl).pipe(response);
+
    request(url, function(err, res, body) {
        var tweets = son.parse(body);
        response.locals = {tweets, name: username};
